94f7fe1d58719f3f3bc14009b867c4d46a44a401
Cappa-z <87878729+Cappa-z@users.noreply.github.com>
2024-12-04 15:46:45 +0000

    Update teamWExApp.py

--- teamWExApp.py
+++ teamWExApp.py
@@ -12,18 +12,44 @@ class player:
     def __init__(self):
         self.name = ''
         self.feeling = ''
-        self.astrological = ''
+        self.exp = ''
         self.position = 'ground'
         self.won = False
         self.solves = 0
 player1 = player()
 
-#############
-# Map Setup	#
-#############
-"""
-You are in ....
+################
+# Title Screen #
+################
+def title_screen_options():
+	#Allows the player to select the menu options, case-insensitive.
+	option = input("> ")
+	if option.lower() == ("play"):
+		setup_game()
+	elif option.lower() == ("quit"):
+		sys.exit()
+	elif option.lower() == ("help"):
+		help_menu()		
+	while option.lower() not in ['play', 'help', 'quit']:
+		print("Invalid command, please try again.")
+		option = input("> ")
+		if option.lower() == ("play"):
+			setup_game()
+		elif option.lower() == ("quit"):
+			sys.exit()
+		elif option.lower() == ("help"):
+			help_menu()
 
-You can (how to move, etc)
-"""
+def title_screen():
+	#Clears the terminal of prior code for a properly formatted title screen.
+	os.system('clear')
+	#Prints the pretty title.
+	print('#' * 45)
+	print('#    Welcome to the application process!    #')
+	print("#           Presented by Team WEx           #")
+	print('#' * 45)
+	print("                 .: Play :.                  ")
+	print("                 .: Help :.                  ")
+	print("                 .: Quit :.                  ")
+	title_screen_options()
 
