8f9dfa6dc6cc043655a65b5e88eff8f6511e296f
Cappa-z <87878729+Cappa-z@users.noreply.github.com>
2024-12-04 15:14:54 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 name = input("What's your name? ")
-birthday = input("What year were you born? ")
-print("Hello", name, "! I can see that you are born in the year", birthday)
+year = input("What year were you born? ")
+age = 2024 - year
+print("Hello", name, "! I can see that you are born in the year ", age)
