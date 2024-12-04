6ea0686ee6283b851e021b8161f81c688603495c
aisha !! <138372120+Aisha-888@users.noreply.github.com>
2024-12-04 15:37:27 +0000

    Update teamWExApp.py
    Added the player class, will change depending on the final design.

--- teamWExApp.py
+++ teamWExApp.py
@@ -6,3 +6,14 @@ import os
 import time
 import random
 screen_width = 100
+
+// Player Class
+class player:
+    def __init__(self):
+        self.name = ''
+        self.feeling = ''
+        self.astrological = ''
+        self.position = 'ground'
+        self.won = False
+        self.solves = 0
+player1 = player()
